bf4fda2ad78e8ea8a71cf6b90d4cd2b3d244f4e7
Akash kumar <akaysharma894@gmail.com>
2021-11-24 23:48:50 +0530

    Added state to update the badge item

--- shopping-cart-app/src/components/Cart/CartButton.js
+++ shopping-cart-app/src/components/Cart/CartButton.js
@@ -1,16 +1,17 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { toggleAction } from "../../store/toggle-cart";
 import classes from "./CartButton.module.css";
 
 const CartButton = (props) => {
   const dispatch = useDispatch();
+  const cartQuantity = useSelector((state) => state.cart.totalQuantity);
   const toggleHandler = () => {
     dispatch(toggleAction.toggle());
   };
   return (
     <button className={classes.button} onClick={toggleHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
