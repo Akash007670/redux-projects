5a60f4c673675dae9843fb840ba30cf0e3aadabc
Akash kumar <akaysharma894@gmail.com>
2021-11-24 23:49:14 +0530

    Added new item to main store

--- shopping-cart-app/src/store/index.js
+++ shopping-cart-app/src/store/index.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import toggleCart from "./toggle-cart";
+import cartSlice from "./cart-slice";
 
 const store = configureStore({
-  reducer: { toggle: toggleCart.reducer },
+  reducer: { toggle: toggleCart.reducer, cart: cartSlice.reducer },
 });
 
 export default store;
