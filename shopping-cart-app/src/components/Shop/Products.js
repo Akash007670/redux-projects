a80a81d420875cab473a53787d66e0dab2b88e39
Akash kumar <akaysharma894@gmail.com>
2021-11-24 23:46:53 +0530

    Added dummy products to show

--- shopping-cart-app/src/components/Shop/Products.js
+++ shopping-cart-app/src/components/Shop/Products.js
@@ -1,16 +1,53 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    price: 100,
+    title: "For whom the Bell Tolls",
+    description: "This book is written by Ernest Hemingway",
+  },
+  {
+    id: "p2",
+    price: 120,
+    title: "Man and Superman",
+    description: "This book is written by G. B. Shaw",
+  },
+  {
+    id: "p3",
+    price: 140,
+    title: "Kanterbary Tells",
+    description: "This book is written by Geofray Chosar",
+  },
+  {
+    id: "p4",
+    price: 160,
+    title: "Paraja",
+    description: "This book is written by Gopinath Mohanty",
+  },
+  {
+    id: "p5",
+    price: 200,
+    title: "She Walk, She Leads",
+    description: "This book is written by Gunjan Jain",
+  },
+];
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
