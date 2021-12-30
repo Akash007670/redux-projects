2a6db616261a70d4d2273aaed82fbc3e4215471c
Akash kumar <akaysharma894@gmail.com>
2021-12-30 23:59:12 +0530

    Added action payload for notification

--- shopping-cart-app/src/store/toggle-cart.js
+++ shopping-cart-app/src/store/toggle-cart.js
@@ -2,10 +2,17 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const toggleCart = createSlice({
   name: "toggle",
-  initialState: { cartIsVisible: false },
+  initialState: { cartIsVisible: false, notification: null },
   reducers: {
     toggle(state) {
       state.cartIsVisible = !state.cartIsVisible;
+    },
+    showNotification(state, action) {
+      state.showNotification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
     },
   },
 });
